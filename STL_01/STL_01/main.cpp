6a3e900c01497dda11705c432e1cbb94abc466f7
joojoo908 <looloo8910@gmail.com>
2025-03-06 11:59:22 +0900

    저장기능

--- STL_01/STL_01/main.cpp
+++ STL_01/STL_01/main.cpp
@@ -10,6 +10,8 @@
 #include <fstream>
 #include <string>
 #include <filesystem>
+#include <algorithm>
+#include <chrono>
 
 void save(std::string_view); //읽기 전용 
 
@@ -31,16 +33,29 @@ void save(std::string_view fileName) {
 		exit(2025);
 	}
 	
-	auto size = std::filesystem::file_size(fileName);
-	std::cout << fileName << "-" << size << "바이트를 저장했습니다." << std::endl;
+	std::cout << fileName << "-" << std::filesystem::file_size(fileName) << "바이트를 저장했습니다." << std::endl;
 
 
 	std::ofstream out{ "2025 1학기 STL 강의저장.txt" , std::ios::app }; //검사 필요x , 파일이 깨졌을 때 자동으로 새로 만듬
 
-	char c;
-	in >> std::noskipws;
-	while ( in >> c ) //고급 입출력 ,캐릭터 외를 모두 무시
-		out << c;
+	//저장시간 파일에 기록
+	auto now = std::chrono::system_clock::now(); //time_point를 얻는다
+	/*using namespace std::chrono_literals;
+	std::cout << now+ 9h << std::endl;*/
+
+	auto utc = std::chrono::system_clock::to_time_t(now); //utc시간으로 변경한다
+	auto lt = std::localtime(&utc);  //지역시간으로 변경
+	auto old = out.imbue(std::locale("ko_KR"));  //out지역을 한국으로
+
+	out << '\n' << '\n';
+	out << "======================================" << '\n';
+	out << "저장한 시간: " << std::put_time(lt, "%c %A") << '\n';
+	out << "======================================" << '\n';
+	out << '\n';
+
+	out.imbue(old);
+	std::copy(std::istreambuf_iterator{ in }, {}, std::ostreambuf_iterator{ out }); //효율적 한줄로 처리 가능
+	
 
 
 }
