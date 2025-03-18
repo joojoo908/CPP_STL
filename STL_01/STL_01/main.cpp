6f1616dc86cd35bd606d011d91af948ec4398bc3
joojoo908 <looloo8910@gmail.com>
2025-03-18 16:09:15 +0900

    최신 정렬

--- STL_01/STL_01/main.cpp
+++ STL_01/STL_01/main.cpp
@@ -1,7 +1,7 @@
-//  - 3 / 11 -
-//
+// ==================================================================
+//  - 3 / 18 -
 // ------------------------------------------------------------------
-// 많은 개수의 자료를 처리 -file에 저장하고 읽어오기
+// 정렬
 // ------------------------------------------------------------------
 
 #include <iostream>
@@ -10,44 +10,44 @@
 #include <random>
 #include<print>
 #include <array> //stl 시퀸스 드라이버 중 하나
+#include<ranges> //최신정렬
 
 #include<fstream>
 //#include<string>
 
 using namespace std;  //기본적으로 하지 말 것, 팀원들과 상의 하에 사용
 
-// 문제: "int 1000개.txt" 에는 int 값 1000개가 저장되어 있다
-// 가장 큰 값을찾아라
+default_random_engine dre{}; 
+uniform_int_distribution uid{0, 999'9999}; 
 
-default_random_engine dre{}; // andom_device{}() 생성 후 호출
-uniform_int_distribution uid{0, 999'9999}; // 게임에서 사용할 일 없음 / 모든 수가 균일하게 나오도록 세팅 //다른 분포를 가지도록 하는것도 가능  //엔진의 결과를 특정한 분포로 가공
+// 파일 "int 1000개.txt" 에는 int 값 1000개가 text 모드로 기록되어 있다.
+//메모리로 읽어 와라, 읽어온 값을 화면에 출력하라 
 
 //--------
 int main()
 //--------
 {
-	// 안전배열이 나오게 된 이유
-	array<int, 1000> a; // int a[1000] 보다 훨신 났다.
-	vector<int> v;
+	
+	array<int, 1000> a{10}; //첫 값만 초기화  //array<int, 1000> a 와 int a[1000]는 메모리 상 같다  
+	// 특징 빈틈없이 물리적으로 연속되어 있다.(continuous (시간의 연속성) ,contiguous (공간의 연속성)) ->stl의 필수
+	a.fill(10); // 모든 값 초기화 
+
+	// 랜덤 억세싱 -> 둘 다 동일한 시간에 엑세스 한다
 
 	ifstream in{ "int 1000개.txt" };
 
-	/*if (not in) {
-		std::cout << "int 1000개.txt" << "을 열 수 없습니다" << std::endl;
-		exit(2025);
-	}
+	copy(istream_iterator<int>{in} /*고급입력(int가 아니거나 형식에 맞지 않는다면 중간에 읽기 동작을 정지함(고급 입력은 좋은것이 아님))*/, {}, a.begin());
 
-	int num;
-	int maxValue{ numeric_limits<int>::min() };
-	while (in >> num) { 
-		print("{:8}", num); 
-		if (maxValue < num) maxValue = num;
-	}
+	/*int i = 0;
+	while (in >> a[i++]);*/
 
-	cout << endl;
-	cout << maxValue << endl;*/
+	cout << *max_element( a.begin(), a.end() ) <<endl;
+	//표준 반복자
 
-	cout << *min_element(istream_iterator<int>{in}, {}) << endl;
+	//cout << *max_element(a.begin(), a.end()) << endl; //과거의 정렬
+	ranges::sort(a); //기본 오름차순 정렬 // 최신의 정렬
+	for (int num : a)
+		print("{:8}", num);
 
-	//save("main.cpp");
+	save("main.cpp");
 }
