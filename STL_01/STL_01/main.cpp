19d8a46ae56392131e62e232cdff4a4bba38903d
joojoo908 <looloo8910@gmail.com>
2025-03-20 10:45:37 +0900

    minmax_element

--- STL_01/STL_01/main.cpp
+++ STL_01/STL_01/main.cpp
@@ -14,6 +14,7 @@
 using namespace std;
 
 // 문제: 진짜 랜덤 파일에는 int 값 10만개가 있다
+// int 10만개 전체를 메모리로 읽어 와라
 // binary 모드로 열었고 write 값으로 int 를 기록하였다.
 // 가장 큰 값과 작은 값을 찾아 화면에 출력하라
 
@@ -22,8 +23,20 @@ int main()
 //--------
 {
 	ifstream in{ "진짜 랜덤.txt" ,ios::binary };
+	if (not in) return 1213;
+
 	array<int, 10'0000> a;
-	copy(istream_iterator<int>{in} , {}, a.begin());
+
+	/*
+	int num;
+	size_t cnt{};
+	while (in.read((char*)&num, sizeof(int)))
+		++cnt;
+	cout << cnt << endl;
+	*/
+
+	in.read((char*)a.data(), a.size() * sizeof(int));
+	//copy(istream_iterator<int>{in} , {}, a.begin());
 
 	/*for (int num : a) {
 		cout << num << endl;
@@ -32,8 +45,15 @@ int main()
 
 	//cout << *max_element(istream_iterator<int>{in}, {}) << endl;
 
-	cout << *max_element(a.begin(),a.end()) << endl;
-	cout << *min_element(a.begin(),a.end()) << endl;
+	cout << *max_element(a.begin(),a.end()) << endl; // o(n)
+	cout << *min_element(a.begin(),a.end()) << endl; // o(n)
 
+	
+	auto [min,max] = minmax_element(a.begin(), a.end()); // std::pair
+	//구조화된 바인딩
+	cout << *min << endl;
+	cout << *max << endl;
+	//cout << *pos.first << endl;
+	//cout << *pos.second << endl;
 	//save("main.cpp");
 }
