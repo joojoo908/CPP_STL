e8cc3c3f7edb7b6c87ecdc8661248aefc6f68d3e
joojoo908 <looloo8910@gmail.com>
2025-03-27 11:31:01 +0900

    c++ 식 정렬

--- STL_01/STL_01/main.cpp
+++ STL_01/STL_01/main.cpp
@@ -8,14 +8,14 @@
 #include <array>
 #include <random>
 #include <ranges>
-//#include <cstdlib>
+#include <algorithm>
 #include <print>
 
 #include "save.h"
 using namespace std;
 
 // 문제 랜덤 int 1000만개를 메모리에 저장하라
-// qsort를 이용하여 오름차순 정렬한다
+// stl의 sort를 이용하여 정렬하라
 // 정렬결과를 앞에서부터 1000개를 화면 출력하라
 
 default_random_engine dre{};
@@ -23,27 +23,28 @@ uniform_int_distribution uid{0,999'9999};
 
 array<int, 1000'0000> a;
 
-int 오름차순(const void* a, const void* b) {
-	int* p = (int*)a;
-	int* q = (int*)b;
-	if (*p > *q)
-		return 1;
-	else if (*p < *q)
-		return -1;
-	return 0;
+bool 오름차순(int a, int b) {
+	return a < b;
+}
+
+bool 내림차순(int a, int b) {
+	return a > b;
 }
 
 //--------
 int main()
 //--------
 {
+	// (*main)(); //가 맞는 표기법
+
 	for (int& val : a) {
 		val = uid(dre);
 	}
 
-	int (*정렬기준)(const void* a, const void* b);
-	정렬기준 = 오름차순;
-	qsort(a.data(), a.size(), sizeof(array<int,a.size()>::value_type), 정렬기준 );
+
+	bool (*정렬기준)(int, int) = 오름차순;
+	sort(a.begin(), a.end() , 정렬기준 ); 
+	//sort(a.end(), a.begin());
 
 	for (int num : a | views::take(100) ) {  //   | : 버티컬 바    //다양한 필터가 있다
 		print("{:8}", num);
