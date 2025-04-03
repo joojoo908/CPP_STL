dae13fc1898b28b317407c8f55b16a4dbd726378
joojoo908 <looloo8910@gmail.com>
2025-04-03 12:20:06 +0900

    사용자 정의 string - 시작

--- STL_01/STL_01/main.cpp
+++ STL_01/STL_01/main.cpp
@@ -1,82 +1,69 @@
 // ------------------------------------------------------------
-//  - 4 / 1 -
+//  - 4 / 3 -
 // ------------------------------------------------------------
 // 중간고사 4/24 목
 // 과제 4/10 과제설명
 // ---------------------------------------
-// callable types - 호출 가능한 타입  --> 모든 호출 가능 타입을 나타낼 수 있는 function 
-// 1. 함수
-// 2. 함수 포인터
-// 3. 람다 - 이름없는 함수 (프로그래머가 부를 일이 없기때문에 이름이 없다)
-// 4. 함수 객체 - 함수호출연산자를 오버로딩한 클래스로 만든 객체(function object)
-// 5. 멤버함수 
+// class STRING - STL 정식 컨테이너처럼 동작하는 자료구조
 // ------------------------------------------------------------
 
+/*
+oop -> 특정 타입을위한 코딩
+generic -> 타입과 관련없는 코딩
+*/
+
 #include <iostream>
-#include<algorithm>
-#include <fstream>
-#include <array>
-#include <ranges>
-#include <print>
+#include <memory>
 
 #include "save.h"
 using namespace std;
 
-class Dog {
+//표준 std::string 과 유사한 동작을 하는 클래스
+class STRING {
 public:
-	void show() {
-		println("{:10} - {:8}", num, name);
+
+	//디폴트를 반드시 코딩
+
+	STRING(const char* s) : len{ strlen(s) } {
+		p.reset();
+		p= make_unique<char[]>(len);
+		memcpy(p.get(), s, len);
 	}
 
-	bool operator<(const Dog& other) const {
-		return (name.length() < other.name.length());
-	}
+	STRING(const STRING& other) : len{ other.len } {
+		p.reset();
+		p = make_unique<char[]>(len);
+		memcpy(p.get(), other.p.get(), len);
+	};
+
+	//copy assignment operator 복사생산 연산자를 반드시 코딩
+
+	size_t size() const { return len; };
 
 private:
-	string name;  //3~50글자 사이의 랜덤 소문자
-	int num;   // 랜덤 int 값
+	size_t len;
+	unique_ptr<char[]> p; //복사가 안되는 클래스
 
-	friend ostream& operator<< (ostream& os, const Dog& dog) {
-		return os << dog.num << " " << dog.name << " ";
+	friend ostream& operator<<(ostream& os, const STRING& str) {
+		for (int i = 0; i < str.len; ++i) {
+			os << str.p[i];
+		}
+		return os;
 	}
-
-	friend istream& operator>> (istream& os, Dog& dog) {
-		os >> dog.num;
-		os >> dog.name;
-		return os ;
-	}
-
 };
 
-// 상황: dog 객체 10만개를 파일 "Dog 십만마리" 에 기록하였다
-// 파일은 다음과 같이 열었다.
-// ofstream out("Dog 십만마리");
-//각 dog 객체는
-// out>>dog; 와 같이 기록하였다
 
-//문제: 틀림없이 dog 객체 10만마리가 기록되었다
-// 파일"Dog 십만마리"를 열어 모든 Dog 객체를 메모리에 저장하라
-//제일 마지막 객체의 정보를 화면에 출력하고 답지에도 출력결과를 적어라
-//메모리에 저장한 Dog 객체르 name길이 기준 오름차순으로 정렬하라
-// 앞에서부터 5000개의 정보를 화면에 출력하라
-
-array<Dog, 10'0000> a;
 
 //--------
 int main()
 //--------
 {
-	ifstream in("Dog 십만마리");
+	STRING s{ "std::string과 유사한 클래스" };
 
-	for (Dog & d :a) {
-		in >> d;
-	}
+	STRING t = s;
 
-	sort(a.begin(), a.end(), [](Dog d1, Dog d2) { return d1 < d2; });
+	cout << s << endl;
+	cout << t << endl;
 
-	for (Dog d : a | views::take(5000)) {
-		d.show();
-	}
-
-	save("main.cpp");
+	//save("main.cpp");
 }
