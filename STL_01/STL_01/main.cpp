5b7b2c2b54f42c679a2f2e26b1613979c86041ad
joojoo908 <looloo8910@gmail.com>
2025-05-01 12:06:04 +0900

    list는 자체 sort / rbegin,rend
    거꾸로 찍을 때에는 rbegin,rend를 이용

--- STL_01/STL_01/main.cpp
+++ STL_01/STL_01/main.cpp
@@ -17,6 +17,9 @@
 #include <iostream>
 #include <deque>
 #include <vector>
+#include <list>
+#include <ranges>
+#include <algorithm>
 
 #include "STRING.h"
 #include "save.h"
@@ -31,34 +34,24 @@ class Test {
 int main()
 //--------
 {
+	list<int> cont{ 1,3,5,7,9,2,4,6,8,10 };
+
+	//리스트의 원소를 오름차순으로 정렬 후 출력
 	
-	//vector와 deque 이 얼마 만큼 인트를 담을 수 있는지 알아보자
-
-	{
-		vector<Test>	v;
-		//cout << "촤대개수 - " << v.max_size() << endl;
-		try{
-			/*while (1) {
-				v.emplace_back();
-			}*/
-		}
-		catch (...) {
-			cout << "벡터가 담을 수 있는 int 수" << v.size(); // 27310
-		}
+	//list<int>::iterator beg = cont.begin(); //이터레이터는 반복자,포인터와 유사한 역할
+	auto beg = cont.begin();
+	++beg; // =beg.operator++();
+	for (auto i = beg; i != cont.end(); ++i) {
+		cout << *i << endl;
 	}
+	cout << endl;
 
-	{
-		deque<Test>	d;
-		//cout << "촤대개수 - " << d.max_size() << endl;
-		try {
-			/*while (1) {
-				d.emplace_back();
-			}*/
-		}
-		catch (...) {
-			cout << "덱 담을 수 있는 int 수" << d.size(); // 46998
-		}
+	cont.sort();
+	//sort(cont);
+
+	for (auto i = cont.begin(); i != cont.end(); ++i) {
+		cout << *i << endl;
 	}
 	
-	save("main.cpp");
+	//save("main.cpp");
 }
